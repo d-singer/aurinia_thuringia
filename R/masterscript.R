e51f0acb4683d9488c627b36683e0084c86c10db
David Singer <david.singer@nw-fva.de>
2024-07-03 15:43:32 +0200

    delete masterscript

--- R/masterscript.R
+++
@@ -1,7 +0,0 @@
-source("R/04_population_models_HAINICH.R", encoding="UTF-8")
-source("R/04a_population_models_HAINICH_males.R", encoding="UTF-8")
-source("R/04b_population_models_HAINICH_females.R", encoding="UTF-8")
-
-source("R/05_population_models_KRIEGBERG.R", encoding="UTF-8")
-source("R/05a_population_models_KRIEGBERG_males.R", encoding="UTF-8")
-source("R/05b_population_models_KRIEGBERG_females.R", encoding="UTF-8")
